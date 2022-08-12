32dbd1ebcaae4f68d281ba1a3a7b7b50a78a34e8
Brandon R <108437078+ItsShiZZa@users.noreply.github.com>
2022-08-12 13:35:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CrossroadsClient
+ 
---
+++ src/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+class Entity
+{
+public:
+	void Print() const { std::cout << "Hello!" << std::endl; }
+};
+
+int main()
+{
+	std::cout << "Hello world" << std::end;
+}
